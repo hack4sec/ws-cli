dfcbb616fed34195ed4d750d97be00f8396e7bda
a.kuzmin <anton.kuzmin@marketgid.com>
2017-04-20 18:11:05 +0300

    Param --headers-file added in all modules which works with HTTP (see docs)

--- main.py
+++ main.py
@@ -81,12 +81,13 @@ args = vars(parser.parse_args(sys.argv[4:]))
 for option in args.keys():
     if args[option] is not None:
         module.options[option].value = args[option].strip()
-    if option == 'headers-file':
-        try:
-            Registry().get('http').load_headers_from_file(args[option].strip())
-        except WSException as e:
-            print "{0}".format(str(e))
-            exit(0)
+
+        if option == 'headers-file':
+            try:
+                Registry().get('http').load_headers_from_file(args[option].strip())
+            except WSException as e:
+                print "{0}".format(str(e))
+                exit(0)
 
 try:
     module.run(action)
